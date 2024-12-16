4cdf46b3f836fd0e244b2c516570e2373d7f3e66
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-12-15 16:59:10 -0800

    UI changes for the To-do List app: CSS updates, edits in List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,18 @@
+// AK changes:
+import React, { useState } from "react";
+//
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function List({ list, heading, onComplete, onRemove }) {
+    // AK changes: add fade effect
+    const [fadingTasks, setFadingTasks] = useState({});
+    //
     const removeTaskHandler = (e, id) => {
         e.preventDefault();
         onRemove(id);
-    }
+    };
 
     const completeTaskHandler = (e, id) => {
         e.preventDefault();
@@ -14,34 +20,59 @@ function List({ list, heading, onComplete, onRemove }) {
         const today = new Date(timeElapsed);
         const completedTask = list.find((task) => task.id === id);
         completedTask.completedDate = today.toLocaleDateString();
-        onComplete(id);
-    }
+        /*onComplete(id);
+    };*/
+        // AK changes
+        setFadingTasks((prev) => ({
+            ...prev,
+            [id]: true,
+        }));
 
+        setTimeout(() => {
+            onComplete(id);
+            setFadingTasks((prev) => ({
+                ...prev,
+                [id]: false,
+            }));
+        }, 1000);
+    };
+    //
+
+    // AK changes: add message when the to-do list is empty//
     return (
         <>
             <h2>{heading}</h2>
-            <ul className="list">
-                {list.map((item) => (
-                    <li key={item.id} className={`list-item ${item.completedDate && "complete-task"}`}>
-                        <div className="task-description">
-                            <p>{item.dateCreated}</p>
-                            <p>{item.description}</p>
-                        </div>
-                        {item.completedDate
-                            ? <p>{item.completedDate}</p>
-                            : <div className="task-actions">
+            {list.length === 0 ? (
+                <div className="empty-list">
+                    Your to-do list is empty! What is next?
+                </div>
+            ) : (
+                <ul className="list">
+                    {list.map((item) => (
+                        <li key={item.id} className={`list-item ${item.completedDate || fadingTasks[item.id] ? "complete-task fade-in" : ""}`}>
+                            <div className="task-description">
+                                <p>{item.dateCreated}</p>
+                                <p2>{item.description}</p2>
+                            </div>
+                            {item.completedDate
+                                ? ( 
+                                <p3>{item.completedDate}</p3>
+                            ) : (
+                            <div className="task-actions">
                                 <button className="btn btn-complete" onClick={(e) => completeTaskHandler(e, item.id)}>
                                     <FontAwesomeIcon icon={faCheck} />
                                 </button>
                                 <button className="btn btn-delete" onClick={(e) => removeTaskHandler(e, item.id)}>
                                     <FontAwesomeIcon icon={faTrash} />
                                 </button>
-                            </div>}
-                    </li>
-                ))}
-            </ul>
+                            </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
-    )
+    );
 }
 
 List.propTypes = {
@@ -54,6 +85,10 @@ List.propTypes = {
     heading: PropTypes.string.isRequired,
     onComplete: PropTypes.func,
     onRemove: PropTypes.func
-}
+};
+
+List.defaultProps = {
+    list: [],
+};
 
 export default List;
